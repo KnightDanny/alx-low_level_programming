bba8f4ce3add8855b8c1cae3d26aba821fcde391
Danny <daniel.habtamut@aau.edu.et>
2023-09-17 19:28:24 +0300

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - Multiplies two numbers
+* @x: The first number
+* @y: The second integer
+* Return: x times y
+*/
+int mul(int x, int y)
+{
+	return (x * y);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int x, int y);
 #endif
